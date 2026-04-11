790ab0385788c230f454946b6afa13baceb8640f
afrid2004 <mdfaisalafrid@gmail.com>
2026-04-11 20:33:18 +0600

    added header

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex bg-white justify-center items-center h-screen">
       <div className="flex justify-center items-center flex-col gap-3 max-w-sm">
         <h1 className="text-8xl font-bold text-gray-800">404</h1>
         <p className="text-2xl text-gray-500 font-medium text-center">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const headerMenus = (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/news">News</NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+      <NavLink to="/blogs">Blogs</NavLink>
+    </>
+  );
+  return (
+    <div className="bg-white shadow-xs py-3">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div className="left-logo w-[150px]">
+            <Link>
+              <img
+                src="/images/logo.png"
+                alt="Dragon News"
+                className="w-full"
+              />
+            </Link>
+          </div>
+          <div className="right-menu">
+            <div className="desktop-menus flex items-center gap-3">
+              {headerMenus}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
